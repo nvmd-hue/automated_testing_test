9898372e040b9e3a3627adbc953505e3290e77d6
j132020 <j132020@tafe.wa.edu.au>
2026-09-14 12:22:03 +0800

    fix to flake8 std

--- test_functions.py
+++ test_functions.py
@@ -4,7 +4,7 @@ from functions import convert_fahrenheit_to_celsius as f2c
 
 def test_add():
     assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
+    assert add("space", "ship") == "spaceship"
 
 
 # uncomment the following test in step 5
