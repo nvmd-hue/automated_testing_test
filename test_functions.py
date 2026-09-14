5e2446e3a4d16d5dc8b5ab719a8f14f672a64fa8
j132020 <j132020@tafe.wa.edu.au>
2026-09-14 11:59:48 +0800

    fix to flake8 std

--- test_functions.py
+++ test_functions.py
@@ -1,17 +1,20 @@
-from functions import add, subtract, multiply
+from functions import add, multiply, subtract
 from functions import convert_fahrenheit_to_celsius as f2c
-import pytest
+
 
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
+
 # uncomment the following test in step 5
 def test_subtract():
-   assert subtract(2, 3) == -1
+    assert subtract(2, 3) == -1
+
 
 def test_multiply():
     assert multiply(2, 4) == 8
+
 
 def test_convert_fahrenheit_to_celsius():
     assert f2c(32) == 0
